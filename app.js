1868434c684b3aa5aaab876a932c0c7f5b7f6ced
Mauricio Romero <romeromauricioinformatica@gmail.com>
2026-06-09 08:36:32 -0300

    agrego manejo de errores con try/catch

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ const boton = document.querySelector('#btn-buscar');
 const resultados = document.querySelector('#resultados');
 
 const buscarPelicula = async () => {
-    const respuesta = await fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input.value}&language=es-ES`,
-    );
-    const peli = await respuesta.json();
-    resultados.innerHTML = '';
-    peli.results.forEach((pelicula) => {
-        const div = document.createElement('div');
-        div.innerHTML = `
+    try {
+        const respuesta = await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${input.value}&language=es-ES`,
+        );
+        const peli = await respuesta.json();
+        resultados.innerHTML = '';
+        peli.results.forEach((pelicula) => {
+            const div = document.createElement('div');
+            div.innerHTML = `
         <img src="https://image.tmdb.org/t/p/w200${pelicula.poster_path}" />
         <p>${pelicula.title}</p>
         `;
-        resultados.appendChild(div);
+            resultados.appendChild(div);
+        });
         input.value = '';
-    });
+    } catch (error) {
+        console.log(error.message);
+        resultados.textContent = 'Ha ocurrido un error';
+    }
 };
 
 boton.addEventListener('click', buscarPelicula);
